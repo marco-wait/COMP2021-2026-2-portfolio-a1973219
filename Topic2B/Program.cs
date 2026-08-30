27e1874082ae23f34d2ec5bd09d0bdbf71953234
marco-wait <a1973219@adelaide.edu.au>
2026-08-30 13:21:39 +0930

    chore: general tidying up

--- Topic2B/Program.cs
+++ Topic2B/Program.cs
@@ -1,51 +1,54 @@
-﻿public class BankAccount
+﻿namespace Topic2B
 {
-    public string Owner { get; set; }
-    public decimal Balance { get; set; }
-
-    public BankAccount(string owner, decimal balance)
+    public class BankAccount
     {
-        Owner = owner;
-        Balance = balance;
-    }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
 
-    public void Deposit(decimal amount)
-    {
-        Balance += amount;
-    }
-
-    public void Deposit(int amount)
-    {
-        Balance += amount;
-    }
-
-    public void Deposit(double amount)
-    {
-        Balance += (decimal)amount;
-    }
-
-    public void Withdraw(decimal amount)
-    {
-        if(Balance < amount)
+        public BankAccount(string owner, decimal balance)
         {
-            throw new ArgumentException("Account balance too low.");
+            Owner = owner;
+            Balance = balance;
         }
-        Balance -= amount;
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += (decimal)amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance < amount)
+            {
+                throw new ArgumentException("Account balance too low.");
+            }
+            Balance -= amount;
+        }
     }
-}
 
-class Program
-{
-    static void Main()
+    class Program
     {
-        BankAccount account1 = new("Marco", 100);
-        Console.WriteLine($"This account belongs to {account1.Owner}.");
-        Console.WriteLine($"This account contains ${account1.Balance}.");
-        // expected outputs should contain: Marco, $100
+        static void Main()
+        {
+            BankAccount account1 = new("Marco", 100);
+            Console.WriteLine($"This account belongs to {account1.Owner}.");
+            Console.WriteLine($"This account contains ${account1.Balance}.");
+            // expected outputs should contain: Marco, $100
 
-        decimal test1 = 50;
-        account1.Deposit(test1);
-        Console.WriteLine($"After depositing $50, this account now contains ${account1.Balance}.");
-        // expected output: $150
+            decimal test1 = 50;
+            account1.Deposit(test1);
+            Console.WriteLine($"After depositing $50, this account now contains ${account1.Balance}.");
+            // expected output: $150
+        }
     }
 }
