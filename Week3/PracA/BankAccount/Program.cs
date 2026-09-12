46240ddf33fb79f41d1fcd158ad098eaa3adcfc0
marco-wait <a1973219@adelaide.edu.au>
2026-09-12 17:48:24 +0930

    feat: CheckingAccount class overrides WIthdraw to include transaction fee

--- Week3/PracA/BankAccount/Program.cs
+++ Week3/PracA/BankAccount/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices.Swift;
-
-public abstract class BankAccount
+﻿public class BankAccount
 {
     public string Owner { get; set; }
     public decimal Balance { get; set; }
@@ -25,7 +23,7 @@ public abstract class BankAccount
         Balance += (decimal)amount;
     }
 
-    public void Withdraw(decimal amount)
+    public virtual void Withdraw(decimal amount)
     {
         if (Balance < amount)
         {
@@ -37,7 +35,7 @@ public abstract class BankAccount
 
 public class SavingsAccount : BankAccount
 {
-    public decimal InterestRate { get; set; } // should be input as a percentage
+    public decimal InterestRate { get; set; } // represents the interest rate as a percentage
     public SavingsAccount(string owner, decimal balance, decimal interest_rate) : base(owner, balance)
     {
         InterestRate = interest_rate;
@@ -46,5 +44,19 @@ public class SavingsAccount : BankAccount
     public void ApplyInterest()
     {
         Balance *= InterestRate / 100 + 1; // converts interest rate value into a useful form, then multiplies account balance
+    }
+}
+
+public class CheckingAccount : BankAccount
+{
+    public decimal TransactionFee { get; set; }
+    public CheckingAccount(string owner, decimal balance, decimal transaction_fee) : base(owner, balance)
+    {
+        TransactionFee = transaction_fee;
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        base.Withdraw(amount + TransactionFee);
     }
 }
