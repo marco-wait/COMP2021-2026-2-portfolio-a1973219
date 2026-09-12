145afc017a56fe1c51ce78bfe706247ff6f8a5ae
marco-wait <a1973219@adelaide.edu.au>
2026-09-12 17:35:05 +0930

    feat: SavingsAccount class inherits from BankAccount

--- Week3/PracA/BankAccount/Program.cs
+++ Week3/PracA/BankAccount/Program.cs
@@ -1,8 +1,10 @@
-﻿public class BankAccount
+﻿using System.Runtime.InteropServices.Swift;
+
+public abstract class BankAccount
 {
     public string Owner { get; set; }
     public decimal Balance { get; set; }
-    public BankAccount(string owner, decimal balance)
+    public BankAccount(string owner, decimal balance) // constructor
     {
         Owner = owner;
         Balance = balance;
@@ -30,5 +32,19 @@
             throw new ArgumentException("Account balance too low.");
         }
         Balance -= amount;
+    }
+}
+
+public class SavingsAccount : BankAccount
+{
+    public decimal InterestRate { get; set; } // should be input as a percentage
+    public SavingsAccount(string owner, decimal balance, decimal interest_rate) : base(owner, balance)
+    {
+        InterestRate = interest_rate;
+    }
+
+    public void ApplyInterest()
+    {
+        Balance *= InterestRate / 100 + 1; // converts interest rate value into a useful form, then multiplies account balance
     }
 }
