62aac670a76f40606d3b4efd98f0e95c2ffc1eb8
marco-wait <a1973219@adelaide.edu.au>
2026-09-12 17:05:13 +0930

    feat: copy over BankAccount class from Week 2

---
+++ Week3/PracA/BankAccount/Program.cs
@@ -0,0 +1,34 @@
+﻿public class BankAccount
+{
+    public string Owner { get; set; }
+    public decimal Balance { get; set; }
+    public BankAccount(string owner, decimal balance)
+    {
+        Owner = owner;
+        Balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void Deposit(int amount)
+    {
+        Balance += amount;
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += (decimal)amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (Balance < amount)
+        {
+            throw new ArgumentException("Account balance too low.");
+        }
+        Balance -= amount;
+    }
+}
