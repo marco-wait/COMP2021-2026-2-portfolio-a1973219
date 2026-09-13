539c674c089f48fb34cc3e5f8cbd18b43e9be78e
marco-wait <a1973219@adelaide.edu.au>
2026-09-13 14:22:25 +0930

    feat: add abstract Employee class

---
+++ Week3/PracB/Employee/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Employee
+{
+    public abstract class Employee
+    {
+        const decimal TaxRate = (decimal)0.2;
+
+        public string Name { get; set; }
+
+        public Employee(string name) // constructor
+        {
+            Name = name;
+        }
+
+        protected abstract decimal CalculatePay();
+    }
+
+    class Program()
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
