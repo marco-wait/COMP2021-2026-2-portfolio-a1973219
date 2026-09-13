e71b8a498d3a817fea30dba523eea3e2ec9330aa
marco-wait <a1973219@adelaide.edu.au>
2026-09-13 18:30:44 +0930

    feat: unit testing for Employee classes

--- Week3/PracB/Employee.Test/UnitTest1.cs
+++ Week3/PracB/Employee.Test/UnitTest1.cs
@@ -1,10 +1,21 @@
-﻿namespace Employee.Test;
-
-public class UnitTest1
+﻿namespace Employee.Test
 {
-    [Fact]
-    public void Test1()
+    public class EmployeeTests
     {
+        [Fact]
+        public void FullTimeEmployee_Calculates_Pay()
+        {
+            var employee_1 = new FullTimeEmployee("John", 100);
+            decimal expected_pay = 100 - (Employee.TaxRate * 100);
+            Assert.Equal(expected_pay, employee_1.CalculatePay());
+        }
 
+        [Fact]
+        public void Contractor_Calculates_Pay()
+        {
+            var employee_2 = new Contractor("Jane", 20, 5);
+            decimal expected_pay = employee_2.Rate * employee_2.Hours * (1 - Employee.TaxRate);
+            Assert.Equal(expected_pay, employee_2.CalculatePay());
+        }
     }
 }
