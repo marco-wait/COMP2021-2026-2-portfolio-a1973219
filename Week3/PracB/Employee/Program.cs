292d55d953f2417e8a7330858d089fad9a44eedd
marco-wait <a1973219@adelaide.edu.au>
2026-09-13 14:42:38 +0930

    feat: add inherited employee classes

--- Week3/PracB/Employee/Program.cs
+++ Week3/PracB/Employee/Program.cs
@@ -5,13 +5,48 @@
         const decimal TaxRate = (decimal)0.2;
 
         public string Name { get; set; }
-
         public Employee(string name) // constructor
         {
             Name = name;
         }
 
         protected abstract decimal CalculatePay();
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        const decimal TaxRate = (decimal)0.2;
+
+        public decimal AnnualSalary { get; set; }
+        public FullTimeEmployee(string name, decimal annual_salary) : base(name)
+        {
+            AnnualSalary = annual_salary;
+        }
+
+        protected override decimal CalculatePay()
+        {
+            decimal Tax = TaxRate * AnnualSalary;
+            return AnnualSalary - Tax;
+        }
+    }
+
+    public class Contractor : Employee
+    {
+        const decimal TaxRate = (decimal)0.2;
+
+        public decimal Rate { get; set; }
+        public decimal Hours { get; set; }
+        public Contractor(string name, decimal rate, decimal hours) : base(name)
+        {
+            Rate = rate;
+            Hours = hours;
+        }
+
+        protected override decimal CalculatePay()
+        {
+            decimal Tax = TaxRate * Rate * Hours;
+            return Rate * Hours - Tax;
+        }
     }
 
     class Program()
