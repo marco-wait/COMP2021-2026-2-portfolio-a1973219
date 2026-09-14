e5677c12cbaa863e24588bac8ca328b31b9dee1e
marco-wait <a1973219@adelaide.edu.au>
2026-09-14 18:53:37 +0930

    feat: fix broken code

--- Week4/PracA/BrokenProgram/Program.cs
+++ Week4/PracA/BrokenProgram/Program.cs
@@ -1,8 +1,8 @@
 ﻿public static class BrokenProgram
 {
-  static void Main(string[] args)
-  {
-    const string input = """
+    static void Main(string[] args)
+    {
+        const string input = """
        23
        + 
        77
@@ -14,34 +14,34 @@
         2
          
       """;
-    int total = 0;
-    List<int> items = [];
-    List<string> symbols = [];
+        int total = 0;
+        List<int> items = [];
+        List<string> symbols = [];
 
-    for (int i = 0; i < input.Split("\n").Length; i += 2)
-    {
-      items.Add(int.Parse(input.Substring((i * 4) + 1, 3)));
+        for (int i = 0; i < input.Split("\n").Length; i += 2)
+        {
+            items.Add(int.Parse(input.Substring(i * 5, 3)));
 
-      if (i != 0)
-      {
-        symbols.Add(input.Split("\n")[i - 1]);
-      }
+            if (i != 0)
+            {
+                symbols.Add(input.Split("\n")[i - 1]);
+            }
+        }
+
+        foreach (var (i, value) in items.Index())
+        {
+            if (i == 0) { total = value; }
+            else if (symbols[i - 1] == " * \r")
+            {
+                total *= value;
+            }
+            else
+            {
+                total += value;
+            }
+        }
+        Console.WriteLine("Total was: " + total);
+        Console.WriteLine("Expected total was: 1120");
+        Console.WriteLine(total == 1120 ? "Your answer was RIGHT!" : "Your answer was WRONG!\nGo and fix it.");
     }
-
-    foreach (var (i, value) in items.Index())
-    {
-      if (i == 0) { total = value; }
-      else if (symbols[i - 1] == "*")
-      {
-        total *= value;
-      }
-      else
-      {
-        total += value;
-      }
-    }
-    Console.WriteLine("Total was: " + total);
-    Console.WriteLine("Expected total was: 1120");
-    Console.WriteLine(total == 1120 ? "Your answer was RIGHT!" : "Your answer was WRONG!\nGo and fix it.");
-  }
 }
