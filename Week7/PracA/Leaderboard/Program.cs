cce1a7a44ee26347d4d13d9bc864eb112953ad75
marco-wait <a1973219@adelaide.edu.au>
2026-09-15 12:46:24 +0930

    feat: simple leaderboard code to demonstrate properties of SortedSet

---
+++ Week7/PracA/Leaderboard/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Leaderboard;
+
+public static class Program
+{
+    public static void DisplayScores(SortedSet<int> scores)
+    {
+        Console.WriteLine("Here are the scores:");
+        foreach (int score in scores)
+        {
+            Console.WriteLine(score);
+        }
+
+        Console.WriteLine($"Highest: {scores.Max}");
+        Console.WriteLine($"Lowest: {scores.Min}");
+    }
+
+    public static void Main(string[] args)
+    {
+        SortedSet<int> scores = [40, 93, 30, 56, 14, 28, 15, 29, 100, 54];
+        DisplayScores(scores);
+
+        scores.Add(1); // add new lowest score
+        DisplayScores(scores);
+
+        scores.Add(100); // add duplicate score
+        DisplayScores(scores);
+    }
+}
